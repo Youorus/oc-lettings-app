7ca6aaa7c10fdaf9b976b7ccd3d7eda991124776
Marc <mtakoumba@gmail.com>
2025-08-16 13:52:40 +0200

    test des viwes et correction des template

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     # Template d'app: oc_lettings_site/lettings/profiles/lettings/index.html
-    return render(request, "index.html")
+    return render(request, "oc_lettings_site/index.html")
 
